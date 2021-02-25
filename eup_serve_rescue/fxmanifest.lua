605e2ecb4758e03d4c364d145d1df93f40b5d090
Ryan <ryanmturnbull@gmail.com>
2021-02-25 23:16:21 +1100

    Created Resource Manifests

---
+++ eup_serve_rescue/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version "cerulean"
+game "gta5"
+
+dependency "eup_law_order" -- This is the name of the Law & Order resource. It is required to be loaded **before** this one, thus why it's a dependency.
+description "EUP Serve & Rescue"
+author "Alex Ashfold, Allen, Jacobmaate, Nacho, Will Len, Bozza"
+version "1.4"
