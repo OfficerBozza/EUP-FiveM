d2f0a33fca5c719372fae5d3eb82ff170000669a
Ryan <ryanmturnbull@gmail.com>
2021-02-28 07:18:07 +1100

    Patch fxmanifest.lua files
    Should correct any 'no fx_version' errors

--- eup_law_order/fxmanifest.lua
+++ eup_law_order/fxmanifest.lua
@@ -1,6 +1,7 @@
-fx_version "cerulean"
-game "gta5"
 
-description "EUP Law & Order"
-author "Alex Ashfold, Allen, Jacobmaate, Nacho, Will Len, Bozza"
-version "8.2"
+fx_version 'cerulean'
+game 'gta5'
+
+description 'EUP Law & Order'
+author 'Alex Ashfold, Allen, Jacobmaate, Nacho, Will Len, Bozza'
+version '8.2'
